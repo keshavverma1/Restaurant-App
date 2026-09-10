aa1d7be963776eeba66fad0aac55fe920ca4ba25
Keshav Verma <kv193111@gmail.com>
2026-09-10 15:56:31 +0530

    Update AdminLayout.jsx

--- Fontend/frontend/src/pages/admin/AdminLayout.jsx
+++ Fontend/frontend/src/pages/admin/AdminLayout.jsx
@@ -151,9 +151,9 @@ const AdminLayout = () => {
                   fontFamily: "Inter, sans-serif",
                 }}
               >
-                Order{" "}
+                Dine{" "}
                 <span className="bg-amber-300 md:text-[26px] text-[20px] text-[#0f172A] rounded-full px-2 py-2">
-                  now
+                  zo
                 </span>
               </h1>
             </Link>
