45e75d997055a703bde812285a9f9c71f9141fd9
Keshav Verma <kv193111@gmail.com>
2026-09-10 15:57:19 +0530

    Update link for my bookings in NavBar component

--- Fontend/frontend/src/components/NavBar.jsx
+++ Fontend/frontend/src/components/NavBar.jsx
@@ -280,7 +280,7 @@ const NavBar = () => {
                           <>
                             {/* MY BOOKING */}
                             <Link
-                              to="/my-bookings"
+                              to="/bookings"
                               onClick={() => setisMenuopen(false)}
                               className="group flex items-center justify-between
                  text-gray-200 text-xl px-6 py-5 rounded-2xl
